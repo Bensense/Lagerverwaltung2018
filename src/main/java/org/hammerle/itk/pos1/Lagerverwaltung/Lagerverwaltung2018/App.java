93d78c578a062b552cd102e7fd4d951bd552fcfc
Bensense <43452220+Bensense@users.noreply.github.com>
2018-10-09 16:55:48 +0200

    FirstCommit

---
+++ src/main/java/org/hammerle/itk/pos1/Lagerverwaltung/Lagerverwaltung2018/App.java
@@ -0,0 +1,13 @@
+package org.hammerle.itk.pos1.Lagerverwaltung.Lagerverwaltung2018;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/hammerle/itk/pos1/Lagerverwaltung/Lagerverwaltung2018/AppTest.java
@@ -0,0 +1,20 @@
+package org.hammerle.itk.pos1.Lagerverwaltung.Lagerverwaltung2018;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
